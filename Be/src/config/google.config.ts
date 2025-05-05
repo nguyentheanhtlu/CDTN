881421b8a611cfa5edce5d298186b6e0620bb4ff
nguyentheanhtlu <theanh28022003@gmail.com>
2025-05-05 09:50:29 +0700

    done

--- Be/src/config/google.config.ts
+++ Be/src/config/google.config.ts
@@ -1,9 +1,7 @@
 import dotenv from 'dotenv';
 dotenv.config();
-
-
 export const googleConfig = {
-    clientID: process.env.GOOGLE_CLIENT_ID || '746029902730-11og3rf9cg9hoor2c3vdqc1tum125tot.apps.googleusercontent.com',
-    clientSecret:  process.env.GOOGLE_CLIENT_SECRET || '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',
-    callbackURL:   process.env.GOOGLE_CALLBACK_URL || 'http://localhost:3000/api/auth/google/callback'
+    clientID: process.env.GOOGLE_CLIENT_ID,
+    clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    callbackURL: process.env.GOOGLE_CALLBACK_URL
 }; 
