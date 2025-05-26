d4385885dfeb4df475658fe8e7d6cbdc3f954e6a
nguyentheanhtlu <theanh28022003@gmail.com>
2025-05-26 15:23:28 +0700

    done

--- Be/src/config/passport.config.ts
+++ Be/src/config/passport.config.ts
@@ -5,9 +5,9 @@ import { User } from '../models/user.model';
 
 export const configurePassport = () => {
     passport.use(new GoogleStrategy({
-        clientID: googleConfig.clientID,
-        clientSecret: googleConfig.clientSecret,
-        callbackURL: googleConfig.callbackURL
+        clientID: googleConfig.clientID || '746029902730-11og3rf9cg9hoor2c3vdqc1tum125tot.apps.googleusercontent.com',
+        clientSecret: googleConfig.clientSecret || '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',
+        callbackURL: googleConfig.callbackURL || 'http://localhost:3000/auth/google/callback'
     }, async (accessToken, refreshToken, profile, done) => {
         try {
             let user = await User.findOne({ googleId: profile.id });
